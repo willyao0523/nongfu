9a35d3598e7ac11b98566824e1b2e9a718d21c20
wyao <willjarvin@gmail.com>
2023-07-04 22:00:54 +0800

    refactor: better repl ux

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"nongfu/lexer"
-	"nongfu/token"
+	"nongfu/parser"
 )
 
 const PROMPT = ">> "
@@ -22,9 +22,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "woops! We run into some error business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
