c90ef7d3c47084ccd4ed5d55447a563baa917f49
wyao <willjarvin@gmail.com>
2023-07-06 22:41:09 +0800

    feat: add evaluate integer

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"nongfu/evaluator"
 	"nongfu/lexer"
 	"nongfu/parser"
 )
@@ -29,8 +30,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, program.String())
+			io.WriteString(out, "\n")
+		}
+		
 	}
 }
 
