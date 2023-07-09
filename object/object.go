de3e72bcea403b1860597cdcd95d9c9ac9d73633
wyao <willjarvin@gmail.com>
2023-07-09 17:48:53 +0800

    feat: add macro system

--- object/object.go
+++ object/object.go
@@ -19,6 +19,7 @@ const (
 	BUILTIN_OBJ = "BUILTIN"
 	ARRAY_OBJ = "ARRAY"
 	HASH_OBJ = "HASH"
+	QUOTE_OBJ = "QUOTE"
 )
 
 type ObjectType string
@@ -213,4 +214,15 @@ type HashPair struct {
 
 type Hashable interface {
 	HashKey() HashKey
+}
+
+type Quote struct {
+	Node ast.Node
+}
+
+func (q *Quote) Type() ObjectType {
+	return QUOTE_OBJ
+}
+func (q *Quote) Inspect() string {
+	return "QUOTE(" + q.Node.String() + ")"
 }
